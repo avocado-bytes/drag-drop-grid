c244d98971fdd19502bbc64c356a213a9565071b
peter.palka <peter.palka@gmail.com>
2023-12-14 11:07:31 +0100

    feat: resize cells

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} f
 import { AfterViewInit, Component, QueryList, TemplateRef, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import {first, firstValueFrom, skip} from 'rxjs';
 import {TileContainerDirective} from './directive/tile-container.directive';
+import {TileDirective} from './directive/tile.directive';
 
 @Component({
   selector: 'app-root',
@@ -29,6 +30,26 @@ export class AppComponent {
     }
   }
 
+  makeLarger(items: string[], cell: string, tile: TileDirective): void {
+    if (tile.width) {
+      tile.width = Math.min(tile.width + 1, 12);
+    } else {
+      const baseDimension = Math.ceil(12 / Math.max(items.length, 0));
+      const updatedDimension = Math.min(baseDimension + 1, 12);
+      tile.width = updatedDimension;
+    }
+  }
+
+  makeSmaller(items: string[], cell: string, tile: TileDirective): void {
+    if (tile.width) {
+      tile.width = Math.max(tile.width - 1, 1);
+    } else {
+      const baseDimension = Math.ceil(12 / Math.max(items.length, 0));
+      const updatedDimension = Math.max(baseDimension - 1, 1);
+      tile.width = updatedDimension;
+    }
+  }
+
   addItem(target?: string[]): void {
     const contents = prompt('Item Contents?');
     if (contents) {
@@ -40,10 +61,12 @@ export class AppComponent {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
   }
 }
