9ccce014303d6d2a293280b1756ebdfbf8f71496
peter.palka <peter.palka@gmail.com>
2023-12-14 13:20:41 +0100

    chore: Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TileContainerDirective } from './directive/tile-container.directive';
 import { TileDirective } from './directive/tile.directive';
-import { RedTileComponent } from './component/red-tile/red-tile.component';
-import { BlueTileComponent } from './component/blue-tile/blue-tile.component';
-import { PurpleTileComponent } from './component/purple-tile/purple-tile.component';
 import { GridComponent } from './component/grid/grid.component';
 import { GridItemDirective } from './directive/grid-item.directive';
 
@@ -19,9 +16,6 @@ import { GridItemDirective } from './directive/grid-item.directive';
     AppComponent,
     TileContainerDirective,
     TileDirective,
-    RedTileComponent,
-    BlueTileComponent,
-    PurpleTileComponent,
     GridComponent,
     GridItemDirective
   ],
